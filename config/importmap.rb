64d7c1b6ef26889fcc3291f7b0c05501b3477365
ari <arikarim146@gmail.com>
2024-01-10 20:05:35 +0300

    Update

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,9 +4,9 @@ pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin_all_from "app/javascript/controllers", under: "controllers"
 pin_all_from "app/javascript/custom", under: "custom"
 
 pin "axios", to: "https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"
+pin_all_from "app/javascript/controllers", under: "controllers"
 
 
