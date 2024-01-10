f1d2af7a9626ab4100285950bb931a3ad48b25a3
ari <arikarim146@gmail.com>
2024-01-10 20:18:06 +0300

    Remove hotwire

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,12 +1,12 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application"
-pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js"
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+# pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+# pin "@hotwired/stimulus", to: "stimulus.min.js"
+# pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/custom", under: "custom"
 
 pin "axios", to: "https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"
-pin_all_from "app/javascript/controllers", under: "controllers"
+# pin_all_from "app/javascript/controllers", under: "controllers"
 
 
