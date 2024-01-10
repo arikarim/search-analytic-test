116cd5ac6d1952e7b093de640f5708b11402627f
ari <arikarim146@gmail.com>
2024-01-10 19:47:53 +0300

    Update

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,11 +1,12 @@
 # Pin npm packages by running ./bin/importmap
 
-pin "application", preload: true
+pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
-pin_all_from "app/javascript/controllers", under: "controllers", preload: true
-pin_all_from "app/javascript/custom", under: "custom", preload: true
+pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin_all_from "app/javascript/controllers", under: "controllers"
+pin_all_from "app/javascript/custom", under: "custom"
 
-pin "axios", to: "https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js", preload: true
+pin "axios", to: "https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"
+
 
