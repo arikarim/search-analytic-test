053b0b3bef2df07c3d1d38f43d0b9ae3aba9bc9a
ari <arikarim146@gmail.com>
2024-01-10 19:40:48 +0300

    Enable javascript

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-// import "custom/main"
+import "custom/main"
 import "@hotwired/turbo-rails"
 import "controllers"
 import "axios"
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,7 +5,7 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers", preload: true
-# pin_all_from "app/javascript/custom", under: "custom", preload: true
+pin_all_from "app/javascript/custom", under: "custom", preload: true
 
 pin "axios", to: "https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js", preload: true
 
